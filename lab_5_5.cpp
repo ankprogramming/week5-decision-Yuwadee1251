2e5526cdb43851052797dd2094dccf615abdc83a
Unknown <yuwadee.dd58@gmail.com>
2017-12-12 12:28:31 +0700

    แก้ข้อ 5 (แต่ยังไม่เสร็จ)

--- lab_5_5.cpp
+++ lab_5_5.cpp
@@ -5,7 +5,7 @@ int main () {
 
   string playerName;
   int score = 0, life = 3;
-
+  char choice1
   // ประกาศตัวแปรเพื่อรับการเลือกคำตอบของผู้เล่น
   // เช่นการเป็น ตัวเลขให้ประกาศเป็น int choice1;
   // ถ้ารับคำตอบเป็นตัวอักษร ให้ประกาศเป็น char choice1;
@@ -14,17 +14,24 @@ int main () {
   cout << "Welcome to Mini Adventure Game." << endl;
   cout << "Please enter your name : ";
 
+  cin >> PlayerName;
+   if (PlayerName.length() >8) {
+        PlayerName.erase (PlayerName.begin()+8, PlayerName.end());
+        cout << "Your name has been changed to " << PlayerName << endl;
+    }
+    else {
+        cout<<PlayerName<< endl;}
   // เขียนคำสั่งรับชื่อ แล้วเก็บในตัวแปร playerName
 
 
   // เขียนคำสั่งแสดงผล คำบรรยายสภาพแวดล้อม สถานการณ์ และคำทักทายที่มีชื่อตัวละคร
   // เช่น cout << "Hello " << playerName << ". You are in Robinson. You are walking happily. Suddenly you realize that forgetting the wallet."
   // << " But you're lazy to get back to get it." << endl;
-
-
+    cout << "Hello " << playerName << ". You are in Robinson. You are walking happily. Suddenly you realize that forgetting the wallet."
+    << " But you're lazy to get back to get it." << endl;
   // เขียนคำสั่งแสดงผล คำถามให้ผู้ใช้ตัดสินใจ
   // เช่น cout << "What should you do? (A)sk for money,(S)teal : ";
-
+    cout << "What should you do? (A)sk for money,(S)teal : ";
 
   // เขียนคำสั่งรับคำตอบจากผู้ใช้ แล้วให้คะแนนหรือทำโทษ จากการตัดสินใจนั้น
   // เช่น
@@ -33,8 +40,17 @@ int main () {
   //        case "S" : score += 10; life -= 1; cout << "You stole money from people. Now you can shopping.";
   //        case "A" : score += 50; cout << "You ask for money from people then you get 100 baht. You are very happy. Now you can shopping."
   //     }
+cin >> choice1;
+   switch(choice1) {
+   case "A" :
+   score += 50;
+   cout << "You ask for money from people then you get 100 baht. You are very happy. Now you can shopping."
+   case "S" :
+   score += 10;
+   life -= 1;
+   cout << "You stole money from people. Now you can shopping.";
 
-
+}
 
   // ฝึกสร้างสถานการณ์และสร้างเงื่อนไข อีก 2 สถานการณ์
 
